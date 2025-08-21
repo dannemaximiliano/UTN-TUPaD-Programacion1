21b6a3b54215308824dca6aa270b9b2a8b5526b6
Maximiliano Danne <dmaxi90@gmail.com>
2025-08-21 20:35:39 -0300

    Create Danne Maximiliano - TP1.py
    Subo practica unidad 1

---
+++ Danne Maximiliano - TP1.py
@@ -0,0 +1,68 @@
+##Danne Maximiliano
+##TP1 Programacion1
+
+#Ejercicio1
+#print("Hola Mundo!")
+
+#Ejercicio2
+#nombre=input("Ingrese su nombre: ")
+#print(f"Hola {nombre}!")
+
+#Ejercicio3
+#nombre=input("Ingrese su nombre: ")
+#apellido=input("Ingrese su apellido: ")
+#edad=input("Ingrese su edad: ")
+#pais=input("Ingrese su lugar de residencia: ")
+#print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {pais}")
+
+#Ejercicio4
+#radio=float(input("Ingrese el valor del radio: "))
+#PI=float(3.14)
+#area= PI * (radio**2)
+#perimetro=PI * radio * 2
+#print(f"El valor del area es {area}, y su perimetro es {perimetro}")
+
+#Ejercicio5
+#segundos=int(input("Ingrese los segundos a convertir en horas: "))
+#horas=float(segundos/3600)
+#print(f"Los segundos {segundos} equivalen a {horas} horas")
+
+#Ejercicio6
+#num=int(input("Ingrese un numero: "))
+#print(f"{num} x 0 = {num*0}")
+#print(f"{num} x 1 = {num*1}")
+#print(f"{num} x 2 = {num*2}")
+#print(f"{num} x 3 = {num*3}")
+#print(f"{num} x 4 = {num*4}")
+#print(f"{num} x 5 = {num*5}")
+#print(f"{num} x 6 = {num*6}")
+#print(f"{num} x 7 = {num*7}")
+#print(f"{num} x 8 = {num*8}")
+#print(f"{num} x 9 = {num*9}")
+#print(f"{num} x 10 = {num*10}")
+
+#Ejercicio7
+#num1=int(input("ingrese un numero distinto a 0: "))
+#num2=int(input("ingrese otro numero distinto a 0: "))
+#print(f"La suma de los numeros es: {num1+num2}")
+#print(f"La resta de los numeros es: {num1-num2}")
+#print(f"La multiplicacion de los numeros es: {num1*num2}")
+#print(f"La division de los numeros es: {num1/num2}")
+
+#Ejercicio8
+#peso=float(input("Ingrese su peso en kilogramos: "))
+#altura=float(input("Ingrese su altura: "))
+#imc=peso/(altura**2)
+#print(f"Su IMC es de {imc}")
+
+#Ejercicio9
+#celsius=float(input("Ingrese la temperatura en Celsius: "))
+#farenheit=9/5*celsius+32
+#print(f"La temperatura en farenheit es {farenheit}")
+
+#Ejercicio10
+#num1=float(input("Ingrese un numero1: "))
+#num2=float(input("Ingrese un numero2: "))
+#num3=float(input("Ingrese un numero3: "))
+#promedio=(num1+num2+num3)/3
+#print(f"El promedio de las notas es {promedio}")
